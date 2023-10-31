7a1b2a00ed11fd38da9638e60032466ab934058a
Demis Bellot <demis.bellot@gmail.com>
2023-10-31 11:14:43 +0800

    Update Configure.Db.Migrations.cs

--- MyApp/Configure.Db.Migrations.cs
+++ MyApp/Configure.Db.Migrations.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using MyApp.Data;
 using MyApp.Migrations;
 using ServiceStack;
 using ServiceStack.Data;
@@ -11,9 +13,18 @@ namespace MyApp;
 public class ConfigureDbMigrations : IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
-        .ConfigureAppHost(afterAppHostInit:appHost => {
+        .ConfigureAppHost(appHost => {
             var migrator = new Migrator(appHost.Resolve<IDbConnectionFactory>(), typeof(Migration1000).Assembly);
-            AppTasks.Register("migrate", _ => migrator.Run());
+            AppTasks.Register("migrate", _ =>
+            {
+                var scopeFactory = appHost.GetApplicationServices().GetRequiredService<IServiceScopeFactory>();
+                using (var scope = scopeFactory.CreateScope())
+                {
+                    using var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                    db.Database.Migrate();
+                }
+                migrator.Run();
+            });
             AppTasks.Register("migrate.revert", args => migrator.Revert(args[0]));
             AppTasks.Run();
         });
