0f4f33b789cfa5f8f7ac4f51a8679310a66b67e2
Darren Reid <darren@reidmail.org>
2023-12-14 09:48:24 +1100

    Add missing copy JS function for markdown pages.

--- MyApp/wwwroot/mjs/app.mjs
+++ MyApp/wwwroot/mjs/app.mjs
@@ -16,6 +16,19 @@ export async function remount() {
     })
 }
 
+/* used in :::sh and :::nuget CopyContainerRenderer */
+globalThis.copy = function (e) {
+    e.classList.add('copying')
+    let $el = document.createElement("textarea")
+    let text = (e.querySelector('code') || e.querySelector('p')).innerHTML
+    $el.innerHTML = text
+    document.body.appendChild($el)
+    $el.select()
+    document.execCommand("copy")
+    document.body.removeChild($el)
+    setTimeout(() => e.classList.remove('copying'), 3000)
+}
+
 document.addEventListener('DOMContentLoaded', () =>
     Blazor.addEventListener('enhancedload', () => {
         remount()
