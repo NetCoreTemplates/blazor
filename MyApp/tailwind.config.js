a0984fc1d32e96987553aa106548afd12804e7c6
Demis Bellot <demis.bellot@gmail.com>
2025-05-14 01:13:57 +0800

    Update tailwind.config.js

--- MyApp/tailwind.config.js
+++ MyApp/tailwind.config.js
@@ -1,15 +1,12 @@
-module.exports = {
-  content: ["./**/*.{html,js,mjs,md,cshtml,razor,cs}", "./Components/**/*.{cshtml,razor}", "./Css.cs"],
-  darkMode: 'class',
+export default {
   theme: {
-      extend: {
-          colors: {
-              'accent-1': '#FAFAFA',
-              'accent-2': '#EAEAEA',
-              danger: 'rgb(153 27 27)',
-              success: 'rgb(22 101 52)',
-          },
+    extend: {
+      colors: {
+        'accent-1': '#FAFAFA',
+        'accent-2': '#EAEAEA',
+        danger: 'rgb(153 27 27)',
+        success: 'rgb(22 101 52)',
       },
-  },
-  plugins: [],
+    },
+  }
 }
